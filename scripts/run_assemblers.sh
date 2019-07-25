57b85bb459fca53a338fd3d64560fded5ada4bff
Rayan  CHIKHI <rchikhi@tars-submit0.tars.cluster.pasteur.fr>
2019-07-25 15:49:21 +0200

    updated running script

--- scripts/run_assemblers.sh
+++ scripts/run_assemblers.sh
@@ -1,16 +1,22 @@
-dataset=initial50x_with_10x_region
-dataset=initial50x_with_5x_region
+dataset=initial50x
+#dataset=initial100x
+#dataset=initial50x_with_10x_region
+#dataset=initial50x_with_5x_region
 
-reads=reads.50x.fq
-reads=reads.100x.fq
-reads=reads.50x.with_10x_region.fq
-reads=reads.50x.with_5x_region.fq
+reads=reads.50x.fq.gz
+#reads=reads.100x.fq.gz
+#reads=reads.50x.with_10x_region.fq.gz
+#reads=reads.50x.with_5x_region.fq.gz
+
+
 
 ~/tools//canu-1.8/Linux-amd64/bin/canu -p canu -d "$dataset"_canu genomeSize=3.565m stopOnLowCoverage=0 useGrid=false minThreads=8 maxThreads=8 maxMemory=10 -pacbio-raw $reads
 
 python2 ~/tools/Flye/bin/flye --pacbio-raw $reads -g 3.565m -o "$dataset"_flye -t 4
 
-~/tools/Unicycler/unicycler-runner.py  -l $reads -o "$dataset"_unicycler -t 4
+
+unicycler  -l $reads -o "$dataset"_unicycler -t 4
+
 
 mkdir "$dataset"_ra
 cd "$dataset"_ra
@@ -25,3 +31,12 @@ wtpoa-cns -t 4 -i out.ctg.lay.gz -fo out.ctg.fa
 minimap2 -t 4 -x map-pb -a out.ctg.fa ../$reads | samtools sort > out.ctg.bam
 samtools view out.ctg.bam | wtpoa-cns -t 4 -d out.ctg.fa -i - -fo assembly.fasta
 cd ..
+
+
+mkdir "$dataset"_miniasm
+cd "$dataset"_miniasm
+minimap2 -x ava-pb -t8 ../$reads ../$reads | gzip -1 > reads.paf.gz
+miniasm -f ../$reads reads.paf.gz > reads.gfa
+awk '/^S/{print ">"$2"\n"$3}' reads.gfa | fold > miniasm_assembly.fa
+cd ..
+
